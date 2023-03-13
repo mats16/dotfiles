015875e72d82a87254d1f38f69ca669e55265541
Kazuki Matsuda <mats.kazuki@gmail.com>
2023-03-13 20:46:11 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-zshrc() {
+oh_my_zsh() {
+    ln -sf $(pwd)/.oh-my-zsh/custom/themes/codespaces.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/codespaces.zsh-theme
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+}
+
+zshrc() {
     ln -sf $(pwd)/.zshrc ~/.zshrc
 }
 
@@ -10,5 +14,6 @@ aws_sso() {
     ln -sf $(pwd)/.aws/config ~/.aws/config
 }
 
+oh_my_zsh
 zshrc
 aws_sso
