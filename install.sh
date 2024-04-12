fbeeae2017730b71bf8960a53dc57fe3fd7f5052
Kazuki Matsuda <mats.kazuki@gmail.com>
2024-04-12 11:14:01 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 oh_my_zsh() {
-    ln -sf $(pwd)/.oh-my-zsh/custom/themes/codespaces.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/codespaces.zsh-theme
+    cp $(pwd)/.oh-my-zsh/custom/themes/codespaces.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/codespaces.zsh-theme
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
 
